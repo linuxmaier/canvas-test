340d75cfc3454ea948ebc3b155e19b08f4bcd9ce
linuxmaier <andrew.eric.maier@gmail.com>
2013-08-27 19:52:39 -0500

    bare bones update

--- jquery_test/form_updates_object.js
+++ jquery_test/form_updates_object.js
@@ -1,1 +1,6 @@
 //uses form to update myRect. Will be called by animation.js
+function updateObject (myRect) {
+	myRect.acceleration = getElementById("accel").value;
+	myRect.friction = getElementById("frict").value;
+	myRect.maxSpeed = getElementById("speed").value;
+}
